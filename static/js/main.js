694dd305b6ca7c69cee006de673559c4e56d99da
Michael <michael.loukeris@icloud.com>
2017-07-04 12:35:10 +0300

    #4 adding basic js code for connecting with WebSocket

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,33 @@
-console.log("hello");
+
+if ("WebSocket" in window)
+{
+
+   // Let us open a web socket
+   var ws = new WebSocket("ws://localhost:8000/realtime");
+   var msg = {
+    // "command": "GET_FLOOR_PLANS",
+    // "command": "GET_LOCATIONS",
+    "command": "GET_FLOOR_PLANS",
+    // "floorPlanId" : 10
+
+  };
+
+   ws.onopen = function()
+   {
+      // Web Socket is connected, send data using send()
+      ws.send(JSON.stringify(msg));
+   };
+
+   ws.onmessage = function (evt)
+   {
+      var received_msg = evt.data;
+      console.log(evt.data);
+   };
+
+}
+
+else
+{
+   // The browser doesn't support WebSocket
+   alert("WebSocket NOT supported by your Browser!");
+}
