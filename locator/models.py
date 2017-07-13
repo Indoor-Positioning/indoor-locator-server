172eceaf62a14ea6214b9ff5029afb1bbe13954c
Michael <michael.loukeris@icloud.com>
2017-07-13 15:00:47 +0300

    Adding Creator model and imagefield in pointofinterest

--- locator/models.py
+++ locator/models.py
@@ -77,9 +77,18 @@ class FingerPrintedLocation(models.Model):
         return related_loc
 
 
+class Creator(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name  + ' ' + self.last_name
+
 
 class PointOfInterest(models.Model):
     name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='pointOfInterest/', blank=True)
+    creator = models.ForeignKey(Creator, null=True)
 
     def __str__(self):
         return self.name
