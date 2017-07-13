da5ef631210423e65fe3a020426074e3be4f4e00
Michael <michael.loukeris@icloud.com>
2017-07-13 15:16:05 +0300

    Adding points of interest

--- locator/admin.py
+++ locator/admin.py
@@ -30,7 +30,6 @@ class FingerPrintAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'metrics_summary',)
     list_filter = ('location__floor_plan', )
 
-
     def metrics_summary(self, obj):
         return "Magnetic field: ({:.2f}, {:.2f}, {:.2f}), Orientation: ({:.2f}, {:.2f}, {:.2f}), Rssi: {: .2f}"\
                 .format(obj.magnetic_x, obj.magnetic_y, obj.magnetic_z, obj.orientation_x,
@@ -43,8 +42,20 @@ class UserLocationAdmin(admin.ModelAdmin):
     ordering = ('timestamp', )
 
 
+class CreatorAdmin(admin.ModelAdmin):
+    model = Creator
+    list_display = ('__str__', 'list_of_points')
+
+    def list_of_points(self, obj):
+        list_of_points_str = ""
+        for point_of_interest in obj.pointofinterest_set.all():
+            list_of_points_str = list_of_points_str + point_of_interest.name + " - "
+        return list_of_points_str
+
+
 admin.site.register(FloorPlan, FloorPanAdmin)
 admin.site.register(PointOfInterest, PointOfInterestAdmin)
 admin.site.register(FingerPrintedLocation, FingerPrintedLocationAdmin)
 admin.site.register(FingerPrint, FingerPrintAdmin)
 admin.site.register(UserLocation, UserLocationAdmin)
+admin.site.register(Creator, CreatorAdmin)
